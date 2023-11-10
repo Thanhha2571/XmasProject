8642b35820ee835941bdb729ac2019ec4dcbe0d5
Thanhha2571 <letranthanhha25072001@gmail.com>
2023-11-11 02:16:44 +0700

    update

--- order-project/src/view/Hero/Hero.js
+++ order-project/src/view/Hero/Hero.js
@@ -4,8 +4,7 @@ import Hero1 from "../../asset/Hero1.jpg";
 import Hero2 from "../../asset/Hero2.jpg";
 import Hero3 from "../../asset/Hero3.jpg";
 import Hero4 from "../../asset/Hero4.jpg";
-import Frame from "../../asset/frame.png";
-import Snow from "../../asset/tuyet.png";
+
 const contentStyle = {
     height: '100%',
     color: '#fff',
@@ -15,20 +14,14 @@ const contentStyle = {
     background: '#364d79',
 };
 const Hero = () => (
-    <Carousel autoplay className='w-full h-full relative p-5'>
-        {/* <img
-            className='w-full h-[600px] relative z-10
-                mobileSmall:h-[400px] mobileSmall:w-[400px]
+    <Carousel autoplay className='w-full h-full'>
+        <div>
+            <h3 style={contentStyle}>
+                <img className='w-full h-[600px] 
+                mobileSmall:h-[400px]
                 laptop:h-[500px]
-                desktop:h-[650px]'
-            src={Snow} /> */}
-        <div className='relative'>
-            <h3 style={contentStyle} className='relative'>
-                <img className='w-full h-[600px]
-                mobileSmall:h-[400px] mobileSmall:w-[400px]
-                laptop:h-[500px]
-                desktop:h-[650px]'
-                    src={Hero1}
+                desktop:h-[650px]' 
+                src={Hero1} 
                 />
             </h3>
         </div>
@@ -38,7 +31,7 @@ const Hero = () => (
                 mobileSmall:h-[400px]
                 laptop:h-[500px]
                 desktop:h-[650px]'
-                    src={Hero2} />
+                 src={Hero2} />
             </h3>
         </div>
         <div>
@@ -46,8 +39,8 @@ const Hero = () => (
                 <img className='w-full h-[600px] 
                 mobileSmall:h-[400px]
                 laptop:h-[500px]
-                desktop:h-[650px]'
-                    src={Hero3} />
+                desktop:h-[650px]' 
+                src={Hero3} />
             </h3>
         </div>
         <div>
@@ -55,8 +48,8 @@ const Hero = () => (
                 <img className='w-full h-[600px] 
                 mobileSmall:h-[400px]
                 laptop:h-[500px]
-                desktop:h-[650px]'
-                    src={Hero4} />
+                desktop:h-[650px]' 
+                src={Hero4} />
             </h3>
         </div>
     </Carousel>
